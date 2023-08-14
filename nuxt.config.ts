18d048679f1c67d12bef8c54091fddf40e18a51d
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-08-14 17:16:09 +0300

    configure nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  ssr: false,
+  devtools: { enabled: true },
+  typescript: { strict: true },
+  experimental: { noVueServer: true },
+  modules: ['modules/prepare-for-yandex-functions'],
+  nitro: {
+    prerender: {
+      crawlLinks: false,
+      routes: ['/']
+    },
+    preset: 'aws-lambda',
+    rollupConfig: {
+      output: { sourcemap: false }
+    }
+  }
 })
