17a4135f548d28d7e9774dc8dd643e1663e5c3ca
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-12-27 16:10:08 +0300

    auto assign AUTH_ORIGIN & NEXTAUTH_URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,20 @@ import { config } from 'dotenv'
 
 const ENV_FILES = [
   process.env.NODE_ENV === 'production' ? 'prod' : 'local',
-  'token'
+  'token',
+  'cloud'
 ]
 
 for (const file of ENV_FILES) config({ path: `./env/.env.${file}` })
 
-process.env.YDB_ACCESS_TOKEN_CREDENTIALS = process.env.TOKEN
+if (process.env.NODE_ENV === 'production') {
+  process.env.AUTH_ORIGIN = `https://${process.env.S3_BUCKET_ID}`
+  process.env.NEXTAUTH_URL = `https://${process.env.S3_BUCKET_ID}`
+} else {
+  process.env.AUTH_ORIGIN = 'http://localhost:3000'
+  process.env.NEXTAUTH_URL = 'http://localhost:3000'
+  process.env.YDB_ACCESS_TOKEN_CREDENTIALS = process.env.TOKEN
+}
 
 export default defineNuxtConfig({
   ssr: false,
