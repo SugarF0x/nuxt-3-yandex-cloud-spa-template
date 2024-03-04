3c287a2afea6805e5d376d05ac6b367897996481
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-03-04 19:28:59 +0300

    typo

--- env.config.js
+++ env.config.js
@@ -3,7 +3,7 @@ import { readFileSync, existsSync } from 'node:fs'
 
 function prepare() {
   // TODO(template): possibly figure out a different handler for linux only local db instance
-  if (!existsSync('./terraform.lock.hcl')) throw new Error('TF lock does not exist. Run `yarn terraform init` first')
+  if (!existsSync('./.terraform.lock.hcl')) throw new Error('TF lock does not exist. Run `yarn terraform init` first')
   if (!existsSync('./terraform.tfstate')) throw new Error('TF state does not exist. Run `yarn terraform apply` first')
 
   const { database_path, ydb_api_endpoint } = JSON.parse(readFileSync("./terraform.tfstate", 'utf-8')).resources.find(e => e.type === 'yandex_ydb_database_serverless').instances[0].attributes
