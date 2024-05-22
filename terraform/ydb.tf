aa7c350e09878b25fca24dbb33b0e918820f18d5
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-05-22 12:56:53 +0300

    ad ydb TODO

--- terraform/ydb.tf
+++ terraform/ydb.tf
@@ -1,3 +1,5 @@
+// TODO: make ydb instance optional (1. might not need it; 2. allow sharing pre existing ones based on directory prefix)
+
 resource "yandex_ydb_database_serverless" "ydb" {
   name      = var.YDB_NAME
   folder_id = var.FOLDER_ID
